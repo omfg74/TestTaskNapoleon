48fb329fda5a0ba40bb5b6aa9058754b5bcf6397
omfg <podsh74@gmail.com>
2018-08-30 16:56:40 +0500

    12

--- app/src/main/java/com/example/omfg/napoleontesttask/TopRecyclerViewItem.java
+++ app/src/main/java/com/example/omfg/napoleontesttask/TopRecyclerViewItem.java
@@ -7,20 +7,4 @@ public class TopRecyclerViewItem {
 
     public TextView textView;
     public ImageView imageView;
-
-    public TextView getTextView() {
-        return textView;
-    }
-
-    public ImageView getImageView() {
-        return imageView;
-    }
-
-    public void setTextView(TextView textView) {
-        this.textView = textView;
-    }
-
-    public void setImageView(ImageView imageView) {
-        this.imageView = imageView;
-    }
 }
